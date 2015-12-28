1e60a625933c0d7519767ee3f98b15bce342a143
alebautt <id_bautista@hotmail.com>
2015-12-28 05:13:42 -0600

    Conekta token

---
+++ ConektaToken/Start.m
@@ -0,0 +1,50 @@
+//
+//  ViewController.m
+//  ConektaToken
+//
+//  Created by Alejandra B on 28/11/15.
+//  Copyright © 2015 alebautista. All rights reserved.
+//
+
+#import "Start.h"
+#import "Conekta.h"
+
+@interface Start ()
+
+@end
+
+@implementation Start
+
+- (void)viewDidLoad {
+    
+    
+    Conekta *conekta = [[Conekta alloc] init];
+    
+    [conekta setDelegate: self];
+    
+    [conekta setPublicKey:@"key_KJysdbf6PotS2ut2"];
+    
+    [conekta collectDevice];
+    
+    Card *card = [conekta.Card initWithNumber: @"4242424242424242" name: @"Julian Ceballos" cvc: @"123" expMonth: @"10" expYear: @"2018"];
+    
+    Token *token = [conekta.Token initWithCard:card];
+    
+    [token createWithSuccess: ^(NSDictionary *data) {
+        NSLog(@"%@", data);
+    } andError: ^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
+
+    
+    
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
